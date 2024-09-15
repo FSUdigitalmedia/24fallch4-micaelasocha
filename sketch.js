019f2493a80b4143806c7e79e16ac7f0a750b7be
MICAELA_SOCHA <ms22bm@fsu.edu>
2024-09-15 16:41:38 -0400

    Disco flashing, mouse cursor sparkle, cant figure out how to make background interchange different colors

--- sketch.js
+++ sketch.js
@@ -1,8 +1,28 @@
 function setup() {
   createCanvas(400, 400);
+  bgColor = color(random(255), random(255), random(255));
 }
 
 function draw() {
-  background(125);
-  circle(width/2, height/2, 40);
+  
+  background(bgColor); 
+  noStroke();
+
+  let randomColor = color(random(255), random(255), random(255));
+ fill(randomColor);
+  //circle(width/2, height/2, 40);
+
+  text('DISCO!', 50, 230);
+  textSize(30);
+  textFont('Impact');
+
+  
+  text('✨', mouseX, mouseY);
+  textSize(100);
+  
+
+}
+
+function mousePressed(){
+  bgColor = (random(255), random(255), random(255));
 }
